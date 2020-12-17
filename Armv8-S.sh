79a93e3bd0b94d05cfd24e01f4740b70393e5142
8688Add <42204083+8688Add@users.noreply.github.com>
2020-12-17 21:07:43 +0800

    Update Armv8-S.sh

--- Armv8-S.sh
+++ Armv8-S.sh
@@ -53,6 +53,8 @@ git clone https://github.com/bin20088/luci-app-koolddns.git package/luci-app-koo
 git clone https://github.com/tuanqing/install-program package/install-program
 #svn co https://github.com/0saga0/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 #svn co https://github.com/8688Add/sirpdboy-package/trunk/luci-app-ddnsto package/luci-app-ddnsto
+rm -rf package/lean/v2ray
+svn co https://github.com/8688Add/Lienol-openwrt-package/trunk/package/v2ray package/v2ray
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/luci-app-gost package/lean/luci-app-gost
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/gost package/lean/gost
 svn co https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom/trunk/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
