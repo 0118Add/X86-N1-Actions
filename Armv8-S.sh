7176f41f63ee358b48f664df7faa054ed0a4db25
8688Add <42204083+8688Add@users.noreply.github.com>
2021-02-12 17:08:06 +0800

    Update Armv8-S.sh

--- Armv8-S.sh
+++ Armv8-S.sh
@@ -56,6 +56,9 @@ git clone https://github.com/Lienol/openwrt-package.git package/openwrt-package
 #git clone https://github.com/bin20088/luci-app-koolddns.git package/luci-app-koolddns
 git clone https://github.com/tuanqing/install-program package/install-program
 git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+rm -rf package/lean/luci-app-frpc
+git clone https://github.com/8688Add/luci-app-frpc-mod.git package/lean/luci-app-frpc
+chmod 0755 package/lean/luci-app-frpc/root/etc/init.d/frp
 #svn co https://github.com/8688Add/sirpdboy-package/trunk/luci-app-ddnsto package/luci-app-ddnsto
 #rm -rf package/lean/v2ray
 #svn co https://github.com/8688Add/Lienol-openwrt-package/trunk/package/v2ray package/v2ray
