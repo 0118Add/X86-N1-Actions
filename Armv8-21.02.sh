dea3c56d6fa7fee887771de5dc5d72b49b47eac8
8688Add <42204083+8688Add@users.noreply.github.com>
2021-03-31 04:04:04 +0800

    Update Armv8-21.02.sh

--- Armv8-21.02.sh
+++ Armv8-21.02.sh
@@ -10,33 +10,11 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
-#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-#sed -i '$a src-git packages https://github.com/garypang13/openwrt-packages' feeds.conf.default
-
-#添加额外软件包
-#svn co https://github.com/Lienol/openwrt/branches/21.02/package/lean package/lean
-#rm -rf package/openwrt-packages/luci-app-unblockmusic
-#svn co https://github.com/Lienol/openwrt/branches/21.02/package/lean/UnblockNeteaseMusic package/UnblockNeteaseMusic
-#svn co https://github.com/Lienol/openwrt/branches/21.02/package/lean/UnblockNeteaseMusicGo package/UnblockNeteaseMusicGo
-#svn co https://github.com/Lienol/openwrt/branches/21.02/package/lean/luci-app-unblockmusic package/luci-app-unblockmusic
-#git clone https://github.com/garypang13/openwrt-packages.git package/openwrt-packages
-#git clone https://github.com/fw876/helloworld.git package/helloworld
-#git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
-#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
-rm -rf luci/themes/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-#git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-git clone https://github.com/tuanqing/install-program package/install-program
-#git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
-#svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/lean/luci-app-cpufreq package/lean/luci-app-cpufreq
-#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2 package/lean/redsocks2
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '$a src-git OpenClash https://github.com/vernesong/OpenClash' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
 # 添加cpufreq
-#sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci-app-cpufreq/Makefile
-#sed -i 's/services/system/g' package/lean/luci-app-cpufreq/luasrc/controller/cpufreq.lua
-#sed -i 's/DEPENDS:=@(.*/DEPENDS:=@(TARGET_bcm27xx||TARGET_bcm53xx||TARGET_ipq40xx||TARGET_ipq806x||TARGET_mvebu||TARGET_sunxi||TARGET_rockchip||TARGET_armvirt) \\/g' package/lean/autocore/Makefile
-
+sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci-app-cpufreq/Makefile
+sed -i 's/services/system/g' package/lean/luci-app-cpufreq/luasrc/controller/cpufreq.lua
+sed -i 's/DEPENDS:=@(.*/DEPENDS:=@(TARGET_bcm27xx||TARGET_bcm53xx||TARGET_ipq40xx||TARGET_ipq806x||TARGET_mvebu||TARGET_sunxi||TARGET_rockchip||TARGET_armvirt) \\/g' package/lean/autocore/Makefile
