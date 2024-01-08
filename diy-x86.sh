afd5f25da17e9a70a12078171d9b99f919e7d383
0118Add <60157610+0118Add@users.noreply.github.com>
2024-01-08 17:37:22 +0800

    Update diy-x86.sh

--- diy-x86.sh
+++ diy-x86.sh
@@ -72,6 +72,7 @@ rm -rf feeds/packages/lang/node
 git clone https://github.com/sbwml/feeds_packages_lang_node-prebuilt feeds/packages/lang/node
 
 # 移除重复软件包
+rm -rf package/helloworld/{hysteria,xray-core}
 #rm -rf package/lean/autocore
 #rm -rf feeds/packages/lang/golang
 rm -rf feeds/packages/net/mosdns
