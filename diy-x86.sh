c6c7e4da09a8c85090009389aaac908e79672fcb
0118Add <60157610+0118Add@users.noreply.github.com>
2024-01-31 21:01:42 +0800

    Update diy-x86.sh

--- diy-x86.sh
+++ diy-x86.sh
@@ -31,6 +31,7 @@ sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 # 去除主页一串的LUCI版本号显示
 #sed -i 's/distversion)%>/distversion)%><!--/g' package/lean/autocore/files/*/index.htm
 #sed -i 's/luciversion)%>)/luciversion)%>)-->/g' package/lean/autocore/files/*/index.htm
+sed -i 's/luciversion)%>)/18.06)%>)/g' package/lean/autocore/files/*/index.htm
 
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 #sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
