77c19302403908a5677611f4310ae25817dc5ddc
0118Add <60157610+0118Add@users.noreply.github.com>
2024-02-10 21:07:53 +0800

    Update diy-x86.sh

--- diy-x86.sh
+++ diy-x86.sh
@@ -150,10 +150,10 @@ sed -i 's/Turbo ACC 网络加速/网络加速/g' feeds/luci/applications/luci-ap
 #sed -i 's/ +libopenssl-legacy//g' package/helloworld/shadowsocksr-libev/Makefile
 
 # 固定shadowsocks-rust版本以免编译失败
-rm -rf package/helloworld/shadowsocks-rust
-wget -P package/helloworld/shadowsocks-rust https://github.com/wekingchen/my-file/raw/master/shadowsocks-rust/Makefile
-rm -rf package/openwrt-passwall/shadowsocks-rust
-wget -P package/openwrt-passwall/shadowsocks-rust https://github.com/wekingchen/my-file/raw/master/shadowsocks-rust/Makefile
+#rm -rf package/helloworld/shadowsocks-rust
+#wget -P package/helloworld/shadowsocks-rust https://github.com/wekingchen/my-file/raw/master/shadowsocks-rust/Makefile
+#rm -rf package/openwrt-passwall/shadowsocks-rust
+#wget -P package/openwrt-passwall/shadowsocks-rust https://github.com/wekingchen/my-file/raw/master/shadowsocks-rust/Makefile
 
 # 调整 Dockerman 到 服务 菜单
 #sed -i 's/"admin",/"admin","services",/g' package/luci-app-dockerman/applications/luci-app-dockerman/luasrc/controller/*.lua
