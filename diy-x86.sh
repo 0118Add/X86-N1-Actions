e1f8ea4a6032aa8447b16d55817526ac63d0a226
0118Add <60157610+0118Add@users.noreply.github.com>
2024-02-23 12:39:18 +0800

    Update diy-x86.sh

--- diy-x86.sh
+++ diy-x86.sh
@@ -36,6 +36,7 @@ sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # 替换文件
 wget -O ./package/kernel/linux/modules/netsupport.mk https://raw.githubusercontent.com/0118Add/X86-N1-Actions/main/general/netsupport.mk
+wget -O ./package/kernel/bpf-headers/Makefile https://raw.githubusercontent.com/0118Add/X86-N1-Actions/main/general/Makefile
 #wget -O ./package/lean/autocore/files/x86/index.htm https://raw.githubusercontent.com/0118Add/OpenWrt/main/images/index.htm
 
 # 修改系统文件
