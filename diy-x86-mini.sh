aab4e7cad2cdb21d59eb9ecd9bd341da1eca4638
0118Add <60157610+0118Add@users.noreply.github.com>
2024-03-18 13:52:11 +0800

    Update diy-x86-mini.sh

--- diy-x86-mini.sh
+++ diy-x86-mini.sh
@@ -98,9 +98,6 @@ sed -i 's/net.netfilter.nf_conntrack_max=.*/net.netfilter.nf_conntrack_max=65535
 # 修正连接数
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
 
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
-
 # node - prebuilt
 rm -rf feeds/packages/lang/node
 git clone https://github.com/8688Add/feeds_packages_lang_node feeds/packages/lang/node
