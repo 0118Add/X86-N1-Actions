b626a5450c55c3685e178214df68d018418e76cf
0118Add <60157610+0118Add@users.noreply.github.com>
2024-03-27 17:23:12 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -94,7 +94,7 @@ echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/c
 # 修改版本为编译日期
 date_version=$(date +"%y.%m.%d")
 orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-sed -i "s/${orig_version}/R${date_version} by BGG/g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/${orig_version}/R${date_version} (by BGG)/g" package/lean/default-settings/files/zzz-default-settings
 
 # node - prebuilt
 rm -rf feeds/packages/lang/node
