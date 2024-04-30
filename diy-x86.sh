4eeea14cb4196f4227be9d91e32236e69c08e5f2
0118Add <60157610+0118Add@users.noreply.github.com>
2024-04-30 17:31:59 +0800

    Update diy-x86.sh

--- diy-x86.sh
+++ diy-x86.sh
@@ -123,8 +123,8 @@ merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/dae
 git clone https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
 git clone https://github.com/0118Add/luci-app-vssr package/luci-app-vssr
 git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
-merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/sing-box
-#git clone https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+#merge_package https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages/sing-box
+git clone https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 git clone https://github.com/fw876/helloworld.git package/helloworld
 #git clone https://github.com/xiaorouji/openwrt-passwall2 package/passwall2
 #git clone https://github.com/sbwml/luci-app-alist.git package/alist
