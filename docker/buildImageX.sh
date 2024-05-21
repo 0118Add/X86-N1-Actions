eaabc5790955963852a8a4432f9413aefe3a2083
0118Add <60157610+0118Add@users.noreply.github.com>
2024-05-21 20:25:08 +0800

    Update buildImageX.sh

--- docker/buildImageX.sh
+++ docker/buildImageX.sh
@@ -2,7 +2,8 @@
 
 TMPDIR=openwrt-armvirt
 ROOTFS=openwrt-armvirt-64-generic-rootfs.tar.gz
- 
+ROOTFS=immortalwrt-armsr-armv8-generic-rootfs.tar.gz
+
 [ -d "$TMPDIR" ] && rm -rf "$TMPDIR"
 mkdir -p "$TMPDIR" && gzip -dc ${ROOTFS} | ( cd "$TMPDIR" && tar xf - && rm -rf ./lib/firmware/* && rm -rf ./lib/modules/*)
 
