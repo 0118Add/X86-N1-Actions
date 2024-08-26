0b3dfa2142f8885c840c03b3dca21226f6317ab2
0118Add <60157610+0118Add@users.noreply.github.com>
2024-08-26 15:37:48 +0800

    Update Lienol-22.03.sh

--- custom/Lienol-22.03.sh
+++ custom/Lienol-22.03.sh
@@ -108,7 +108,7 @@ rm -rf target/linux/generic/pending-5.10/613-netfilter_optional_tcp_window_check
 git clone -b luci-smartdns-dev --single-branch https://github.com/lwb1978/openwrt-passwall package/passwall-luci
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
 rm -rf package/passwall-packages/{chinadns-ng,hysteria,shadowsocks-rust,v2ray-geodata}
-git_sparse_clone v5 https://github.com/sbwml/openwrt_helloworld package/passwall-packages chinadns-ng hysteria shadowsocks-rust v2ray-geodata
+git_sparse_clone v5 https://github.com/sbwml/openwrt_helloworld chinadns-ng hysteria shadowsocks-rust v2ray-geodata
 #git clone https://github.com/fw876/helloworld.git package/helloworld
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-autoreboot luci-app-diskman luci-app-ramfree luci-app-zerotier luci-app-smartdns
 #git clone https://github.com/xiaorouji/openwrt-passwall2 package/passwall2
