d956bf3460fcf2ebc2984f379efe6234693e2020
0118Add <60157610+0118Add@users.noreply.github.com>
2024-11-14 10:31:17 +0800

    Update Lienol-24.10.sh

--- custom/Lienol-24.10.sh
+++ custom/Lienol-24.10.sh
@@ -133,7 +133,6 @@ git clone -b neko --depth 1 https://github.com/Thaolga/luci-app-nekoclash packag
 #git clone https://github.com/gngpp/luci-theme-design package/luci-theme-design
 rm -rf feeds/packages/net/smartdns
 cp -rf ${GITHUB_WORKSPACE}/general/smartdns feeds/packages/net
-cp -rf ${GITHUB_WORKSPACE}/general/luci-app-turboacc package/luci-app-turboacc
 
 git clone --depth 1 -b test https://github.com/m0eak/homeproxy package/homeproxy
 sed -i "s/ImmortalWrt/OpenWrt/g" package/homeproxy/po/zh_Hans/homeproxy.po
