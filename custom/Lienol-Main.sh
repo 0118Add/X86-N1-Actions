3e15c75e87dbb9b4b430e324f254e8ee8d37d456
0118Add <60157610+0118Add@users.noreply.github.com>
2025-02-16 16:26:38 +0800

    Update Lienol-Main.sh

--- custom/Lienol-Main.sh
+++ custom/Lienol-Main.sh
@@ -137,9 +137,9 @@ sed -i "s/ImmortalWrt/OpenWrt/g" package/homeproxy/po/zh_Hans/homeproxy.po
 sed -i "s/ImmortalWrt proxy/OpenWrt proxy/g" package/homeproxy/htdocs/luci-static/resources/view/homeproxy/{client.js,server.js}
 
 # mihomo
-git clone https://github.com/morytyann/OpenWrt-mihomo  package/openwrt-mihomo
-sed -i 's/MihomoTProxy/Mihomo/g' package/openwrt-mihomo/luci-app-mihomo/po/zh_Hans/mihomo.po
-sed -i 's/MihomoTProxy/Mihomo/g' package/openwrt-mihomo/luci-app-mihomo/root/usr/share/luci/menu.d/luci-app-mihomo.json
+git clone https://github.com/nikkinikki-org/OpenWrt-nikki  package/OpenWrt-nikki
+#sed -i 's/MihomoTProxy/Mihomo/g' package/openwrt-mihomo/luci-app-mihomo/po/zh_Hans/mihomo.po
+#sed -i 's/MihomoTProxy/Mihomo/g' package/openwrt-mihomo/luci-app-mihomo/root/usr/share/luci/menu.d/luci-app-mihomo.json
 
 # turboacc
 #git clone --depth 1 -b luci https://github.com/chenmozhijin/turboacc package/turboacc
