2667efc460db36c30b9a526c9ae157fca4a3eb90
0118Add <60157610+0118Add@users.noreply.github.com>
2025-07-31 19:22:14 +0800

    Update Lienol-24.10.sh

--- custom/Lienol-24.10.sh
+++ custom/Lienol-24.10.sh
@@ -80,9 +80,9 @@ sed -i 's/net.netfilter.nf_conntrack_max=.*/net.netfilter.nf_conntrack_max=65535
 # 修正连接数
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
 
-# golang 1.24
+# golang 1.25
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
 
 # 移除重复软件包
 rm -rf feeds/other/lean/autocore
