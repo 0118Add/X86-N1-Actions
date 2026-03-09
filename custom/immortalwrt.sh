ba98f15a13764df8e13048350f1e1c873d623b4a
0118Add <60157610+0118Add@users.noreply.github.com>
2026-03-09 17:09:42 +0800

    Update immortalwrt.sh

--- custom/immortalwrt.sh
+++ custom/immortalwrt.sh
@@ -213,14 +213,10 @@ sed -i '18d' feeds/luci/collections/luci-nginx/Makefile
 sed -i '17d' feeds/luci/collections/luci/Makefile
 sed -i '16s/ \\$//' feeds/luci/collections/luci/Makefile
 
-# 预编译 node
-rm -rf feeds/packages/lang/node
-git clone --depth=1 -b packages-24.10 https://github.com/sbwml/feeds_packages_lang_node-prebuilt feeds/packages/lang/node
-
 # ADD PKG 部分
 #rm -rf feeds/packages/utils/coremark
-git_sparse_clone master https://github.com/QiuSimons/OpenWrt-Add luci-app-ap-modem
-sed -i 's/+@KERNEL_DEBUG_INFO_BTF/+vmlinux-btf/' package/custom/openwrt-einat-ebpf/Makefile
+git_sparse_clone master https://github.com/QiuSimons/OpenWrt-Add openwrt-einat-ebpf
+sed -i 's/+@KERNEL_DEBUG_INFO_BTF/+vmlinux-btf/' package/openwrt-einat-ebpf/Makefile
 git clone https://github.com/QiuSimons/vmlinux-btf package/vmlinux-btf
 
 # rust
